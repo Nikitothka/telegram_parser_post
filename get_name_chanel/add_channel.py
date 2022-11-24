f2669d2ac8e41cf2c33379a5e5cec0b8309fe476
nikita <nik14626@mail.ru>
2022-11-24 17:13:28 +0300

    add join Channel

--- get_name_chanel/add_channel.py
+++ get_name_chanel/add_channel.py
@@ -3,7 +3,6 @@ import time
 from pyrogram import Client, filters
 from dotenv import load_dotenv
 import os
-from have_attribute import have_entities, have_entities_text
 from database import db
 import asyncio
 from pyrogram.errors import FloodWait
@@ -11,32 +10,36 @@ from pyrogram.errors import FloodWait
 load_dotenv()
 
 
-
 async def main():
     load_dotenv()
     cnt = 0
-    async with Client("my_account", api_id=(os.getenv("API_ID")), api_hash=(os.getenv("API_HASH"))) as app:
+    try:
+        async with Client("my_account", api_id=(os.getenv("API_ID")), api_hash=(os.getenv("API_HASH"))) as app:
 
-        username_list = await db.get_names()
-        for username_dict in username_list:
-            username = username_dict['username']
-            chat_info = (await app.get_chat(username))
-            try:
-                time.sleep(10)
-                await app.join_chat(username)
-                await db.channels_update_id(channel_id=chat_info.id,
-                                            title=chat_info.title,
-                                            username=username)
-                print(cnt)
-                cnt +=1
-            except FloodWait as e:
-                await asyncio.sleep(e.value)
+            username_list = await db.get_names()
+            for username_dict in username_list:
+                username = username_dict['username']
+                chat_info = (await app.get_chat(username))
+                try:
+                    time.sleep(10)
+                    await app.join_chat(username)
+                    await db.channels_update_id(channel_id=chat_info.id,
+                                                title=chat_info.title,
+                                                username=username)
+                    print(cnt)
+                    cnt += 1
+                except FloodWait as e:
+                    print(e.value)
+                    await asyncio.sleep(e.value)
 
-            except Exception as ex:
-                print(ex,username)
+                except Exception as ex:
+                    print(ex, username)
 
 
 
-        # (await a.leave())
+    except FloodWait as e:
+        print(e.value)
+        await asyncio.sleep(e.value)
+
 
 asyncio.run(main())
